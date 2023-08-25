ce35270ccf137f7829aab5fadbad1a5577d3f878
Brooke Anderson <andebroo@oregonstate.edu>
2023-08-24 21:47:34 -0700

    Created outline of the classes to start building the game. Classes include CreateGame, UpdateGame, and PlayGame.

---
+++ penalty-game.py
@@ -0,0 +1,18 @@
+# Title: Basic Soccer Penalty Shoot-Out Game
+# Author: Brooke Anderson
+# Description: A basic soccer penalty shoot-out game programmed in Python
+
+
+class CreateGame:
+    """a class to create the game"""
+    pass
+
+
+class CreatePlayers:
+    """a class to create/update the players"""
+    pass
+
+
+class PlayGame:
+    """a class to update and play the game"""
+    pass
