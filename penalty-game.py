d15471b38bec873854b504babe84f4e41653989c
Brooke Anderson <andebroo@oregonstate.edu>
2023-09-06 09:53:02 -0700

    Created an InvalidPlayerError exception to help the player identify when they have asked the program to remove a player that is not listed on the team.

--- penalty-game.py
+++ penalty-game.py
@@ -3,6 +3,11 @@
 # Description: A basic soccer penalty shoot-out game programmed in Python
 
 
+class InvalidPlayerError(Exception):
+    """user-defined exception to a player not listed on the team"""
+    pass
+
+
 class CreateGame:
     """a class to create the game"""
     pass
